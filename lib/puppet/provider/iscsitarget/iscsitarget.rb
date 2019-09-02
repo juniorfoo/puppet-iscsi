04cd455ee193098b83760dab87ead49817511b06
developerbart <bart.hermans@os3.nl>
2019-09-02 14:23:09 +0200

    Update iscsitarget.rb
    Fixed syntax errors.

--- lib/puppet/provider/iscsitarget/iscsitarget.rb
+++ lib/puppet/provider/iscsitarget/iscsitarget.rb
@@ -3,7 +3,6 @@ Puppet::Type.type(:iscsitarget).provide(:iscsitarget) do
  commands :iscsiadm => "iscsiadm"
 
  def istatus  
-  desc "The try-catch statement is used to filter out nonzero exit codes when no targets are logged into"
 
   begin
    iscsioutput = iscsiadm('-m', 'session')
@@ -11,7 +10,6 @@ Puppet::Type.type(:iscsitarget).provide(:iscsitarget) do
    return false 
   end
 
-  desc "Check whether IQN is currently logged into"
   if iscsioutput.include? resource[:iqn]
    return true 
   else
@@ -20,14 +18,12 @@ Puppet::Type.type(:iscsitarget).provide(:iscsitarget) do
  end
 
  def create
-  desc "First discover before logging in"
   iscsiadm('--mode', 'discoverydb', '--type', 'sendtargets', '--portal', resource[:ip], '--discover')
 
   iscsiadm('--mode', 'node', '--targetname', resource[:iqn], '--portal', resource[:ip], '--login')
  end
 
  def destroy
-  desc "Just log out"
   iscsiadm('--mode', 'node', '--targetname', resource[:iqn], '--portal', resource[:ip], '--logout')
  end
 
